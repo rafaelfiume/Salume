cdedd2e32b787165f3c7f843f406d85da471b714
Rafael Fiume <rafaelfiume@Rafaels-MacBook-Pro.local>
2012-05-02 03:22:17 -0300

    Update of Commons Lang library to version 3.1

--- SalumeSupplier/src/main/java/com/github/rafaelfiume/salume/domain/order/Order.java
+++ SalumeSupplier/src/main/java/com/github/rafaelfiume/salume/domain/order/Order.java
@@ -3,8 +3,8 @@ package com.github.rafaelfiume.salume.domain.order;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.lang.builder.EqualsBuilder;
-import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 public class Order {
 
--- SalumeSupplier/src/main/java/com/github/rafaelfiume/salume/domain/order/Product.java
+++ SalumeSupplier/src/main/java/com/github/rafaelfiume/salume/domain/order/Product.java
@@ -2,8 +2,8 @@ package com.github.rafaelfiume.salume.domain.order;
 
 import java.math.BigDecimal;
 
-import org.apache.commons.lang.builder.EqualsBuilder;
-import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 public class Product {
 
