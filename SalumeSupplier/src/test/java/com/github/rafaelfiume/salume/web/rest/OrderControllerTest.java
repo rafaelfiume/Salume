4dd80d0f1f2d4277b5ab97a29e69d0b3866f8028
Rafael Fiume <rafaelfiume@Rafaels-MacBook-Pro.local>
2012-05-02 02:58:36 -0300

    First implementation of OrderController unit test with spring-test-mvc

--- SalumeSupplier/src/test/java/com/github/rafaelfiume/salume/web/rest/OrderControllerTest.java
+++ SalumeSupplier/src/test/java/com/github/rafaelfiume/salume/web/rest/OrderControllerTest.java
@@ -1,26 +1,35 @@
 package com.github.rafaelfiume.salume.web.rest;
 
-import org.junit.Assert;
+import static org.springframework.test.web.server.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.server.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.server.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.server.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.server.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.server.setup.MockMvcBuilders.standaloneSetup;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.github.rafaelfiume.salume.domain.order.Order;
-
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("OrderControllerTest-context.xml")
+// @ContextConfiguration defaults to OrderControllerTest-context.xml in the same package
+@ContextConfiguration
 public class OrderControllerTest {
 
-    @Autowired
-    private OrderController oController;
- 
     @Test
-    public void testFindOrder() {
-        Order expected = new Order();
-        // TODO
-        Assert.assertEquals(expected, oController.findOrder(1));
+    public void testFindOrder() throws Exception {
+        standaloneSetup(new OrderController())
+                .build()
+                .perform(get("/order/2").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().type(MediaType.APPLICATION_JSON))
+                .andExpect(
+                        content()
+                                .string("{\"id\":1,\"items\":[{\"quantity\":2,\"product\":{\"name\":\"Mortadela\",\"description\":\"Carne defumada\",\"price\":23.4}},{\"quantity\":5,\"product\":{\"name\":\"Salume Speciale\",\"description\":\"Gusto Incredibile\",\"price\":53.9}}]}"))
+                .andExpect(jsonPath("$.id").value(1)).andDo(print());
     }
-    
+
+
 }
