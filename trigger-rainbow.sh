4f6ab5890e1cc0582c1c6e4a20e587e41ea3ab68
Rafael Fiume <rafaelfiume@github.com>
2015-10-02 15:30:12 -0300

    Still TravisCI... Fingers crossed

--- trigger-rainbow.sh
+++ trigger-rainbow.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-#!/bin/bash
+echo -e "Triggering Rainbow...\n"
 
 # Get last child project build number
 BUILD_NUM=$(curl -s 'https://api.travis-ci.org/repos/rafaelfiume/Rainbow/builds' | grep -o '^\[{"id":[0-9]*,' | grep -o '[0-9]' | tr -d '\n')
 
+echo -e "Rainbow build number is $BUILD_NUM\n"
+
 # Restart last child project build
-curl -X POST https://api.travis-ci.org/builds/$BUILD_NUM/restart --header "Authorization: token "$AUTH_TOKEN
+curl -i \
+     -X POST \
+     -H "Content-Type: application/json" \
+     -H "Accept: application/json" \
+     -H "Authorization: token $ACCESS_TOKEN" \
+     https://api.travis-ci.org/builds/$BUILD_NUM/restart
