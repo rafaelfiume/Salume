899d4390ca033aa164cafad3aaf333554ca25e40
Rafael Fiume <rafaelfiume@github.com>
2015-10-08 17:29:40 -0300

    Addapted DataSourceConfig to accept URI with query strings

--- Supplier/src/main/java/com/rafaelfiume/salume/config/DataSourceConfig.java
+++ Supplier/src/main/java/com/rafaelfiume/salume/config/DataSourceConfig.java
@@ -1,6 +1,7 @@
 package com.rafaelfiume.salume.config;
 
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -9,21 +10,29 @@ import javax.sql.DataSource;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+import static org.apache.commons.lang3.StringUtils.isNoneEmpty;
+
 @Configuration
 public class DataSourceConfig {
 
     @Bean
     public DataSource dataSource() throws URISyntaxException {
-        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+        final URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
-        String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+        final StringBuilder dbUriBuilder = new StringBuilder("jdbc:postgresql://")
+                .append(dbUri.getHost())
+                .append(':').append(dbUri.getPort())
+                .append(dbUri.getPath());
+
+        final String query = dbUri.getQuery();
+        if (isNoneEmpty(query)) {
+            dbUriBuilder.append("?").append(query);
+        }
 
         BasicDataSource basicDataSource = new BasicDataSource();
-        basicDataSource.setUrl(dbUrl);
-        basicDataSource.setUsername(username);
-        basicDataSource.setPassword(password);
+        basicDataSource.setUrl(dbUriBuilder.toString());
+        basicDataSource.setUsername(dbUri.getUserInfo().split(":")[0]);
+        basicDataSource.setPassword(dbUri.getUserInfo().split(":")[1]);
 
         return basicDataSource;
     }
