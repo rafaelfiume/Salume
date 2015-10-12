fbd18e5b6fdd607b91b18117610d2e18319f179f
Rafael Fiume <rafaelfiume@github.com>
2015-10-12 15:28:07 -0300

    Configuing external access to db when recreating via db-integrator maven plugin

--- Db-Integrator/src/main/java/com/rafaelfiume/db/config/DataSourceConfig.java
+++ Db-Integrator/src/main/java/com/rafaelfiume/db/config/DataSourceConfig.java
@@ -1,26 +1,40 @@
 package com.rafaelfiume.db.config;
 
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.commons.lang3.Validate;
 
 import javax.sql.DataSource;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+import static java.lang.System.getenv;
+import static org.apache.commons.lang3.StringUtils.isNotEmpty;
+
 public class DataSourceConfig {
+
+    private static final String DATABASE_URL = "DATABASE_URL";
 
     // TODO RF 10/10/2015 Triplicated from Supplier module
 
     public DataSource dataSource() throws URISyntaxException {
-        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+        Validate.notNull(getenv(DATABASE_URL), "missing %s environment variable", DATABASE_URL);
 
-        String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+        final URI dbUri = new URI(getenv(DATABASE_URL));
+
+        final StringBuilder dbUriBuilder = new StringBuilder("jdbc:postgresql://")
+                .append(dbUri.getHost())
+                .append(':').append(dbUri.getPort())
+                .append(dbUri.getPath());
+
+        final String query = dbUri.getQuery();
+        if (isNotEmpty(query)) {
+            dbUriBuilder.append("?").append(query);
+        }
 
         BasicDataSource basicDataSource = new BasicDataSource();
-        basicDataSource.setUrl(dbUrl);
-        basicDataSource.setUsername(username);
-        basicDataSource.setPassword(password);
+        basicDataSource.setUrl(dbUriBuilder.toString());
+        basicDataSource.setUsername(dbUri.getUserInfo().split(":")[0]);
+        basicDataSource.setPassword(dbUri.getUserInfo().split(":")[1]);
 
         return basicDataSource;
     }
