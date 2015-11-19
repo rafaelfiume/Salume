80e293981d2bfad7924417c46560353a92b9b710
Rafael Fiume <rafael.fiume@yahoo.it>
2015-11-19 20:14:25 -0200

    Readme changes and new script, plus a small test clean up

--- README.md
+++ README.md
@@ -16,6 +16,14 @@ Then run it with:
 
     $java -Dprofile=dev $JAVA_OPTS -cp Supplier/target/classes:Supplier/target/dependency/* com.rafaelfiume.salume.SupplierApplication
 
-The port can be specified using --PORT (e.g. --PORT=8081)
-
 Use --debug to see DEBUG level messages.
+
+## Environment Variables
+
+The following variables are used in this project:
+
+#### Application
+1) $DATABASE_URL is mandatory and points to the database. It must be set since there's no default value.
+2) $DATABASE_RELEASE_URL is optional, but recommended.
+Specify a value to it to run db scripts in the release environment (staging, prod).
+3) $PORT is mandatory, but usually set by Heroku. When necessary $PORT can be specified using --PORT=8081, for instance.
--- Supplier/advise.sh
+++ Supplier/advise.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
 
-echo -e "Requesting product advise for magic profile"
+base_message="\nRequesting product advise for magic profile"
 
- echo `curl \
+server_option="${1}"
+case $server_option in
+    "prod") SUPPLIER_BASE_URL="http://app.rafaelfiume.com/salume/supplier"
+        echo -e "$base_message in production"
+    ;;
+    "stag") SUPPLIER_BASE_URL="$SUPPLIER_STAGING_URL"
+        echo -e "$base_message in staging"
+    ;;
+    "dev" ) SUPPLIER_BASE_URL="http://localhost:8081/salume/supplier"
+        echo -e "$base_message in staging"
+    ;;
+esac
+
+echo `curl \
      -X GET \
      -H "Accept: application/xml" \
-     http://localhost:8081/salume/supplier/advise/for/Healthy` | xmllint --format -
+     "$SUPPLIER_BASE_URL"/advise/for/Healthy` | xmllint --format -
