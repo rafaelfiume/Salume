44c75ab8c5c96318883868552a86b2e7789d7c1e
Rafael Fiume <rfiume@equalexperts.com>
2017-07-28 11:05:45 +0100

    #15 : Updates version number in staging too
    @rafaelfiume

--- update-build-number.sh
+++ update-build-number.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-echo -e "Updating build number...\n"
+function update_build_number() {
+    application=$1
+    curl -n -s \
+        -X PATCH \
+        -H "Content-Type: application/json" \
+        -H "Accept: application/vnd.heroku+json; version=3" \
+        -H "Authorization: Bearer $HEROKU_API_TOKEN" \
+        -d "{ \"BUILD_NUMBER\" : $TRAVIS_BUILD_NUMBER }" \
+        https://api.heroku.com/apps/$application/config-vars > /dev/null
+}
 
-curl -n -s \
-     -X PATCH \
-     -H "Content-Type: application/json" \
-     -H "Accept: application/vnd.heroku+json; version=3" \
-     -H "Authorization: Bearer $HEROKU_API_TOKEN" \
-     -d "{ \"BUILD_NUMBER\" : $TRAVIS_BUILD_NUMBER }" \
-     https://api.heroku.com/apps/salume-prod/config-vars > /dev/null
+main() {
+    echo -e "Updating build number in production...\n"
+    update_build_number "salume-prod"
+    echo -e "Build number updated. \n"
 
-echo -e "Build number updated \n"
+    echo -e "Updating build number in staging...\n"
+    update_build_number "salume-staging"
+    echo -e "Done updating build number. \n"
+}
+main
+
