56be227ebb5605b399cd26b3e0c820503ebec590
Rafael Fiume <rfiume@equalexperts.com>
2017-07-28 11:05:45 +0100

    #15 : Updates version number in the environment where the app is being deployed into
    @rafaelfiume

--- update-build-number.sh
+++ update-build-number.sh
@@ -12,13 +12,17 @@ function update_build_number() {
 }
 
 main() {
-    echo -e "Updating build number in production...\n"
-    update_build_number "salume-prod"
-    echo -e "Build number updated. \n"
+    if [ "$TRAVIS_BRANCH" == "master" ]; then
+        echo -e "Updating build number in production..."
+        update_build_number "salume-prod"
+        echo -e "Done updating build number in production."
+    fi
 
-    echo -e "Updating build number in staging...\n"
-    update_build_number "salume-staging"
-    echo -e "Done updating build number. \n"
+    if [ "$TRAVIS_BRANCH" == "staging" ]; then
+        echo -e "Updating build number in staging..."
+        update_build_number "salume-staging"
+        echo -e "Done updating build number in staging."
+    fi
 }
 main
 
