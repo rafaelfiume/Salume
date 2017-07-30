7b5a4591b68d9157e422c414b3a996c5f022a289
Rafael Fiume <rafael.fiume@yahoo.it>
2017-07-30 11:20:49 +0100

    #16 : Uses Heroku Spring Datasource config vars to create datasource
    @rafaelfiume

--- Salume-Db/src/main/java/com/rafaelfiume/salume/db/config/DataSourceConfig.java
+++ Salume-Db/src/main/java/com/rafaelfiume/salume/db/config/DataSourceConfig.java
@@ -2,7 +2,6 @@ package com.rafaelfiume.salume.db.config;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import org.apache.commons.lang3.Validate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
@@ -10,22 +9,24 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
-import java.net.URI;
-import java.net.URISyntaxException;
 
-import static java.lang.String.format;
 import static java.lang.System.getenv;
-import static org.apache.commons.lang3.StringUtils.isNoneEmpty;
 
 @Configuration
 @EnableTransactionManagement
 public class DataSourceConfig {
 
-    private static final String DATABASE_URL = "DATABASE_URL";
+    private static final String SPRING_DATASOURCE_URL = "SPRING_DATASOURCE_URL";
+    private static final String SPRING_DATASOURCE_USERNAME = "SPRING_DATASOURCE_USERNAME";
+    private static final String SPRING_DATASOURCE_PASSWORD = "SPRING_DATASOURCE_PASSWORD";
 
     @Bean
     public DataSource dataSource() {
-        return dataSource(getenv(DATABASE_URL));
+        return dataSource(
+                getenv(SPRING_DATASOURCE_URL),
+                getenv(SPRING_DATASOURCE_USERNAME),
+                getenv(SPRING_DATASOURCE_PASSWORD)
+        );
     }
 
     @Bean
@@ -34,30 +35,11 @@ public class DataSourceConfig {
     }
 
     // Used by Db-Integrator plugin
-    public DataSource dataSource(String databaseUrl) {
-        Validate.notNull(databaseUrl, "missing %s environment variable", DATABASE_URL);
-
-        final URI dbUri;
-        try {
-            dbUri = new URI(databaseUrl);
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(format("error creating URI from %s", databaseUrl), e);
-        }
-
-        final StringBuilder dbUriBuilder = new StringBuilder("jdbc:postgresql://")
-                .append(dbUri.getHost())
-                .append(':').append(dbUri.getPort())
-                .append(dbUri.getPath());
-
-        final String query = dbUri.getQuery();
-        if (isNoneEmpty(query)) {
-            dbUriBuilder.append("?").append(query);
-        }
-
+    public DataSource dataSource(String url, String user, String pass) {
         final HikariConfig config = new HikariConfig();
-        config.setJdbcUrl(dbUriBuilder.toString());
-        config.setUsername(dbUri.getUserInfo().split(":")[0]);
-        config.setPassword(dbUri.getUserInfo().split(":")[1]);
+        config.setJdbcUrl(url);
+        config.setUsername(user);
+        config.setPassword(pass);
         config.addDataSourceProperty("cachePrepStmts", "true");
         config.addDataSourceProperty("prepStmtCacheSize", "250");
         config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
